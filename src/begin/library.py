8e02a4e03922f712c519c8c8f08345ca598ac2c3
David Chamont <chamont@in2p3.fr>
2017-01-16 19:07:10 +0100

    the natural place of unit tests is with the tested code

--- src/begin/library.py
+++ src/begin/library.py
@@ -208,4 +208,35 @@ def get_celestial_objects_from_pixels(x, y, wcs, angle):
 
     return objs, out, req
 
+if __name__ == '__main__':
+
+    # test_Simbad
+    objects = library.get_objects(1.0, 1.0, 0.1)
+    for object in objects:
+        print('{} ({})'.format(object, objects[object]))
+    if len(objects) != 14:
+        print('error')
+
+    # test_WCS
+    header = None
+    try:
+        with fits.open('../data/dss.19.59.54.3+09.59.20.9 10x10.fits') as data_fits:
+            try:
+                data_fits.verify('silentfix')
+                header = data_fits[0].header
+            except ValueError as err:
+                logging.error('Error: %s', err)
+    except EnvironmentError as err:
+        logging.error('Cannot open the data fits file. - %s', err)
+
+    w = library.WCS(header)
+    ra, dec = w.convert_to_radec(0, 0)
+
+    print(ra, dec)
+
+    if abs(ra - 300.060983768) > 1e-5:
+        print('error')
+
+    if abs(dec - 9.90624639801) > 1e5:
+        print('error')
 
