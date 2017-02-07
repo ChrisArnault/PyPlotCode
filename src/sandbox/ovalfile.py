334b68a9442c7d9067cde0f63d61e1117419f63f
David Chamont <chamont@in2p3.fr>
2017-02-07 16:59:06 +0100

    move/hide pj 4 & 5

--- src/sandbox/ovalfile.py
+++ src/sandbox/ovalfile.py
@@ -26,8 +26,6 @@ targets = [
     { "name" : "pj1_background_suppress" , "command" : "python pj1_background_suppress.py -b ../../data/fits/NPAC.fits"  },
     { "name" : "pj2_cluster_slider" , "command" : "python pj2_cluster_slider.py -b ../../data/fits/NPAC.fits"  },
     { "name" : "pj3_file_selector" , "command" : "python pj3_file_selector.py -b ../../data/fits/NPAC.fits"  },
-    { "name" : "pj4_threaded_clusters" , "command" : "python pj4_threaded_clusters.py -b ../../data/fits/NPAC.fits"  },
-    { "name" : "pj5_animate_clusters" , "command" : "python pj5_animate_clusters.py -b ../../data/fits/NPAC.fits"  },
 ]
 
 # filters
