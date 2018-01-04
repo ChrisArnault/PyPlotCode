b01c19e23c225dcad50adaac675ee7fc1cea8227
Arnault <arnault@lal.in2p3.fr>
2018-01-04 11:20:44 +0100

    ex2> change the solution for ex2 to install steps => split the signature

--- src/solutions/ex2_background.py
+++ src/solutions/ex2_background.py
@@ -6,13 +6,21 @@ import sys
 sys.path.append('../skeletons')
 import matplotlib.pyplot as plt
 import lib_args, lib_fits, lib_background
-
+import numpy as np
+import lib_model
 
 def main():
 
     file_name, interactive = lib_args.get_args()
     header, pixels = lib_fits.read_first_image(file_name)
-    background, dispersion, _ = lib_background.compute_background(pixels)
+
+    z = lib_model.gaussian_model(np.arange(-10.0, 10.0, 1.0), 1.0, 1.0, 1.0)
+    print('RESULT: test_gaussian = {:3f}'.format(np.sum(z)))
+
+    y, x = lib_background.build_pixel_histogram(pixels, 200)
+    print('RESULT: histogram = {:5d}'.format(np.sum(y)))
+
+    background, dispersion, mx, y, x = lib_background.compute_background((y, x))
 
     # console output
     print('RESULT: background = {:d}'.format(int(background)))
@@ -21,7 +29,17 @@ def main():
     # graphic output
     if interactive:
         _, axis = plt.subplots()
-        axis.imshow(pixels)
+        # axis.imshow(pixels)
+
+        z = lib_model.gaussian_model(x, 1.0, background/mx, dispersion/mx)
+
+        plt.plot(x, y, 'b+:', label='data')
+        plt.plot(x, z, 'r.:', label='fit')
+        plt.legend()
+        plt.title('Flux distribution')
+        plt.xlabel('Amplitude')
+        plt.ylabel('Frequence')
+
         plt.show()
 
     return 0
