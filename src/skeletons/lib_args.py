60ea672c3bac24acf5a97957836e8374820c494e
Michel Jouvin <jouvin@lal.in2p3.fr>
2018-01-09 10:44:50 +0100

    lib_args: fix handling of file argument
    - Do not check DEFAULT_DATA_PATH existence if the path has been specified
    - Check file existence and raise an exception if it does not exist

--- src/skeletons/lib_args.py
+++ src/skeletons/lib_args.py
@@ -13,26 +13,11 @@ def get_args():
     parser.add_argument('file', nargs='?', help='fits input file')
     args = parser.parse_args()
 
-    # establish the default path and file values
-    DEFAULT_DATA_PATH = os.environ.get('DATAPATH')
-    DEFAULT_DATA_FILE = 'common'
-
-    if DEFAULT_DATA_PATH == None:
-        if os.path.exists('../../data/fits'):
-            DEFAULT_DATA_PATH = '../../data/fits'
-        elif os.path.exists('../data/fits'):
-            DEFAULT_DATA_PATH = '../data/fits'
-        elif os.path.exists('../data'):
-            DEFAULT_DATA_PATH = '../data'
-        else:
-            print('No data path found')
-            exit(1)
-
     interactive = not args.b
 
     if not args.file:
         # if no file name given on the command line
-
+        DEFAULT_DATA_FILE = 'common'
         if interactive:
             file = input('file name [%s]? ' % DEFAULT_DATA_FILE)
             if len(file) == 0:
@@ -50,7 +35,23 @@ def get_args():
     if file.rfind('/') == -1 and file.rfind('\\') == -1:
         # when an explicit path is not provided, prepend the default path
         # other wise don't touch it
+        # establish the default path and file values
+        DEFAULT_DATA_PATH = os.environ.get('DATAPATH')
+        if DEFAULT_DATA_PATH == None:
+            if os.path.exists('../../data/fits'):
+                DEFAULT_DATA_PATH = '../../data/fits'
+            elif os.path.exists('../data/fits'):
+                DEFAULT_DATA_PATH = '../data/fits'
+            elif os.path.exists('../data'):
+                DEFAULT_DATA_PATH = '../data'
+            else:
+                print('No default data path found', file=sys.stderr)
+                exit(1)
+
         file = DEFAULT_DATA_PATH + '/' + file
+
+    if not os.path.exists(file):
+        raise FileNotFoundError('Image file ({}) not found'.format(file))
 
     # we don't test if the file actually exists.
     # thus we expect that this test will occur at open time (perhaps using a try clause)
