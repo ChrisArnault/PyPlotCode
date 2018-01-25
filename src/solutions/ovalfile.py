817688fd312325843a2c08880ee622f5e7fc1e32
David Chamont <chamont@in2p3.fr>
2018-01-25 17:51:42 +0100

    utilise le nouveau mecanisme de key/value pour les sorties à comparer

--- src/solutions/ovalfile.py
+++ src/solutions/ovalfile.py
@@ -6,8 +6,8 @@ targets = [
     {"name": "ex1_read_image" , "command" : "python ex1_read_image.py -b ../../data/fits/common.fits"  },
     {"name": "ex2_background" , "command" : "python ex2_background.py -b ../../data/fits/common.fits"  },
     {"name": "ex3_clusters"   , "command" : "python ex3_clusters.py -b ../../data/fits/common.fits"    },
-    {"name": "ex4_coordinates", "command" : "python ex4_coordinates.py -b ../../data/fits/common.fits" },
-    {"name": "ex5_find_stars" , "command" : "python ex5_find_stars.py -b ../../data/fits/common.fits"  },
+    {"name": "ex4_stars", "command" : "python ex4_stars.py -b ../../data/fits/common.fits" },
+    {"name": "ex5_show_stars" , "command" : "python ex5_show_stars.py -b ../../data/fits/common.fits"  },
     
     {"name": "lib_fits", "command" : "python lib_fits.py" },
     {"name": "lib_cluster", "command" : "python lib_cluster.py"  },
@@ -27,5 +27,9 @@ run_filters_out = [
     {"name": "time", "re": "^.*execution time.*$", "apply": "%"},
 ]
 
-diff_filters_in = [ {"name": "all", "re": "^(.+)$", "apply": "%"} ]
+diff_filters_in = [
+    {"name": "exercices", "re": "^\s*RESULT\s*:\s*(.+?)\s*=\s*(.+?)\s*$", "apply": "ex%"},
+    {"name": "default", "re": "^(.+)$", "apply": "lib%"},
+    {"name": "default", "re": "^(.+)$", "apply": "pj%"},
+]
 
