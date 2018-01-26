7578fde83d9d6f9236ace1c4e995636262286d20
David Chamont <chamont@in2p3.fr>
2018-01-26 09:14:19 +0100

    renae steps 4 and 5

--- data/md5/gitmv.sh
+++ data/md5/gitmv.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for ex in ex1_read_image ex2_background ex3_clusters ex4_coordinates ex5_find_stars
-do
-    #for txt in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
-    for txt in 19
+#for ex in ex1_read_image ex2_background ex3_clusters ex4_coordinates ex5_find_stars
+#for ex in ex1_read_image ex2_background ex3_clusters ex4_coordinates ex5_find_stars
+#do
+    for txt in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22
     do
-        git mv $ex.NPAC$txt.ref.tmp $ex.NPAC02.ref
-        git mv $ex.NPAC$txt.md5.tmp $ex.NPAC02.md5
+        git mv ex4_coordinates.NPAC$txt.ref ex4_stars.NPAC$txt.ref
+        git mv ex5_find_stars.NPAC$txt.ref ex5_show_stars.NPAC$txt.ref
+        #git mv $ex.NPAC$txt.ref.tmp $ex.NPAC02.ref
+        #git mv $ex.NPAC$txt.md5.tmp $ex.NPAC02.md5
     done
-done
+#done
